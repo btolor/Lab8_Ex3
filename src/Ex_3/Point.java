3efe209f74cef31c3985c134ffcc2434d7f0d708
B <brandontolor@yahoo.com>
2018-10-30 13:34:12 -0600

    Created a Point class with two private variables, getters and setters for those variables, and a Point class constructor.

---
+++ src/Ex_3/Point.java
@@ -0,0 +1,35 @@
+
+package Ex_3;
+
+public class Point {
+    
+    //Private Variables...
+    private double xCoordinate, yCoordinate;
+    
+    //Constructor...
+    public Point(double xCoordinate,double yCoordinate) {  
+        this.xCoordinate = xCoordinate;
+        this.yCoordinate = yCoordinate;
+    }
+
+    //Point Class Methods...
+    //Getters & Setters...
+    public double getxCoordinate() {
+        return xCoordinate;
+    }
+
+    public void setxCoordinate(double xCoordinate) {
+        this.xCoordinate = xCoordinate;
+    }
+
+    public double getyCoordinate() {
+        return yCoordinate;
+    }
+
+    public void setyCoordinate(double yCoordinate) {
+        this.yCoordinate = yCoordinate;
+    }
+    
+    
+    
+}
