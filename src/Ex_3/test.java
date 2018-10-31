ba6c67f7219c8ecd2f8c5d4432e0266e9a631b8c
B <brandontolor@yahoo.com>
2018-10-30 19:34:20 -0600

    Created a Point class constructor, instantiated a Test class scanner and other methods to get input from the user, added variables to the Test class main method, and called the instantiated a Point object and called one of its methods in a print statement...

--- src/Ex_3/test.java
+++ src/Ex_3/test.java
@@ -1,10 +1,45 @@
 package Ex_3;
 
-public class test {
+import java.util.Scanner;
 
+public class test {
+    //Test Class Scanner...
+    static Scanner scan = new Scanner(System.in);
+    
+    //Test Class Method...
+    //X-Coordinates...
+    public static double xCoordinate(){
+        double x;
+        System.out.print("Enter the X-coordinates: ");
+        while (!scan.hasNextDouble()) {
+            scan.next();
+            System.out.println("Invalid entry. Try Again!");
+        }
+        x = scan.nextDouble();
+        System.out.println();
+        return x;
+    }
+    //Y-Coordinates...
+    public static double yCoordinate(){
+        double y;
+        System.out.print("Enter the Y-coordinates: ");
+        while (!scan.hasNextDouble()) {
+            scan.next();
+            System.out.println("Invalid entry. Try Again!");
+        }
+        y = scan.nextDouble();
+        System.out.println();
+        return y;
+    }
     
     public static void main(String[] args) {
-        System.out.println("Project started on 10/30/2018");
+        //Main method variables...
+        double x,y;
+        String point;
+        x = xCoordinate();
+        y = yCoordinate();
+        Point xyAxis = new Point(x, y);
+        System.out.println(xyAxis.axisPoint());
     }
     
 }
